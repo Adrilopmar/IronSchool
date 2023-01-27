78c6a59814ae015145bd0ad59432983a97a0664a
Adrià <adria.lop.mar@gmail.com>
2023-01-27 19:58:26 +0100

    added create teacher in commands library

---
+++ src/test/java/com/ironhack/ironschool/main/utils/CommandsTest.java
@@ -0,0 +1,34 @@
+package com.ironhack.ironschool.main.utils;
+
+import com.ironhack.ironschool.main.classes.Teacher;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommandsTest {
+    private Commands commands = new Commands();
+    private Map<String,Teacher> teacherListTest;
+    private Teacher teacherTest1;
+
+    @BeforeEach
+    void setUp() {
+        teacherListTest = new HashMap();
+        Teacher teacherTest1 = new Teacher("Chals",20);
+        commands.createTeacher("Chals",20);
+        teacherListTest.put(teacherTest1.getTeacherId(),teacherTest1);
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createTeacher_CreateTeacherCorrectData_TeacherAddedToTeachersList() {
+        assertEquals(teacherListTest,commands.getTeacherList());
+    }
+}
